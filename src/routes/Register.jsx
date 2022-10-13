97ca8ec5c55838e4082ceabf3eae473a4859082d
bitia <bitia_r_24@hotmail.com>
2022-10-13 23:55:53 +0200

    instalacion react hook form - useform en register.jsx

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,45 +1,53 @@
 import { useContext, useState } from "react"
 import { UserContext } from "../context/UserProvider"
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
 
-const [email, setEmail] = useState('bitia_83@test.com')
-  const [password, setPassword] = useState('123123')
+const navegate = useNavigate()
+ const { registerUser } = useContext(UserContext);
 
-  const navegate = useNavigate()
-
-  const { registerUser } = useContext(UserContext);
+  const { register, handleSubmit, formState: { errors } } = useForm()
+  
+  const onSubmit = data => console.log(data)
 
   
-  const handleSubmit = async (e) => {
+  
+  // const handleSubmit = async (e) => {
    
-    e.preventDefault();
-    console.log('procesando form: ', email, password);
-    try {
-      await registerUser(email, password)
-      console.log("Usuario creado");
-      navegate('/')
-    } catch (error) {
-      console.log(error.code)
-    }
-  };
+  //   e.preventDefault();
+  //   console.log('procesando form: ', email, password);
+  //   try {
+  //     await registerUser(email, password)
+  //     console.log("Usuario creado");
+  //     navegate('/')
+  //   } catch (error) {
+  //     console.log(error.code)
+  //   }
+  // };
 
   return (
     <>
       <h1>Register</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
           type="email"
           placeholder="ingrese email"
-          value={email}
-        onChange= {e => setEmail(e.target.value)}
+         {...register("email", {required:true})}
+        />
+        {
+          errors.email &&<p>Campo obligatorio</p>
+        }
+          <input
+          type="password"
+          placeholder="ingrese password"
+        {...register("password")}
         />
           <input
           type="password"
           placeholder="ingrese password"
-          value={password}
-        onChange= {e => setPassword(e.target.value)}
+        {...register("repassword")}
         />
           <button type="submit">Register</button>
       </form>
